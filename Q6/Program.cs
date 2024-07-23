28b8ee5014ef0a6d9a59b47f93203484c7f9caee
Danial Rafi <rfidanial@gmail.com>
2024-07-23 17:05:45 +0100

    working on file reading again

---
+++ Q6/Program.cs
@@ -0,0 +1,16 @@
+﻿// going to put everything in an array to start with from the txt file
+
+string[] lines = File.ReadAllLines("dogOwners4.txt");
+
+
+
+
+//Natalie Hersey:[CASTIEL:Red,PUNKIN:Yellow]
+
+
+
+
+foreach (string line in lines)//going through each of the lines 
+{
+    string[] values = line.Split("")
+}
